7745d5dd059192f2356ad3fc8b8f9362c26041ed
JunPlayGround <j.masaki.23.5.946@4nonome.com>
2023-07-02 17:32:12 +0900

    CheckBox実装

--- src/components/Atoms/CheckBox/index.jsx
+++ src/components/Atoms/CheckBox/index.jsx
@@ -1,26 +1,28 @@
 import React from "react";
 import styled from "styled-components";
 import COLOR from "../../../variables/color";
-import pencil from "../../../assets/svg/pencil.svg";
+import check from "../../../assets/svg/check.svg";
 
 const CheckBox = (props) => {
   return <Check onClick = {props.onClick}>
-    <img src={pencil} width={"20px"} />
+    
   </Check>;
 };
 export default CheckBox;
 
 const Check = styled.button`
   background-color: transparent;
-  border: none;
+  border-color: ${COLOR.LIGHT_GRAY};
   margin: 0;
   padding: 0;
   width: 20px;
   height: 20px;
   cursor: pointer;
+  border-radius: 2px;
 
   :hover{
-    background-color: ${COLOR.LIGHT_GRAY_TRANSLUCENT};
-    border-radius: 10px;
+    background-clip: padding;
+    background-image: url(${check});
+    background-size: contain;
   }
 `;
