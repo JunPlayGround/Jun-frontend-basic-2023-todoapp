df0733cb9bc0de31be1421286b303cb49212a68e
JunPlayGround <j.masaki.23.5.946@4nonome.com>
2023-07-02 16:39:50 +0900

    ファイルの場所の修正

--- src/components/Atoms/AddTaskButton/index.jsx
+++ src/components/Atoms/AddTaskButton/index.jsx
@@ -3,12 +3,12 @@ import styled from "styled-components";
 import COLOR from "../../../variables/color";
 import pencil from "../../../assets/svg/pencil.svg";
 
-const AddEditButton = (props) => {
+const AddTaskButton = (props) => {
   return <PlusButton onClick = {props.onClick}>
     <img src={pencil} width={"20px"} />
   </PlusButton>;
 };
-export default AddEditButton;
+export default AddTaskButton;
 
 const PlusButton = styled.button`
   background-color: transparent;
