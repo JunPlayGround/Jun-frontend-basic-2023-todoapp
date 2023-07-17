56177b16fcbbd8388bd1b007d09adb7ebe978f54
JunPlayGround <j.masaki.23.5.946@4nonome.com>
2023-07-17 17:47:43 +0900

    見た目のみ作成

--- src/components/Atoms/Input/index.jsx
+++ src/components/Atoms/Input/index.jsx
@@ -1,16 +1,24 @@
 import React from "react";
 import styled from "styled-components";
 import COLOR from "../../../variables/color";
-import pencil from "../../../assets/svg/pencil.svg";
+import TEXTS from "../../../variables/texts";
 
 const InputTask = (props) => {
+
+
   return <InputFrame onClick = {props.onClick}>
-    <img src={pencil} width={"20px"} />
+    
   </InputFrame>;
 };
 export default InputTask;
 
 const InputFrame = styled.input`
   width: 100%;
+  height: 20px;
   background-color: ${COLOR.BLACK};
+  ${TEXTS.S};
+  border: none;
+  padding: 0px 4px;
+  color: ${COLOR.WHITE};
+  outline: none;
 `;
