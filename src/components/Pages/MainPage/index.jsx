84c438ec93595747977b9f11cdd5ffdae44e2f25
JunPlayGround <j.masaki.23.5.946@4nonome.com>
2023-08-16 23:10:58 +0900

    MainPageの作成

--- src/components/Pages/MainPage/index.jsx
+++ src/components/Pages/MainPage/index.jsx
@@ -1,12 +1,28 @@
 import React, { useRef, useState, useEffect } from "react";
 import styled from "styled-components";
-import FONTFAMILY from "../../../variables/font_family";
-import TEXTS from "../../../variables/texts";
-import COLOR from "../../../variables/color";
+import BREAKPOINT from "../../../variables/breakpoint";
 import Title from "../../Atoms/Title/index";
 import TodoCards from "../../Organisms/TodoCard/index";
+import COLOR from "../../../variables/color";
+
 
 const MainPage = () => {
-
+    return (
+        <AllWrapper>
+            <Title />
+            <TodoCards />
+        </AllWrapper>
+    );
 }
 export default MainPage;
+
+const AllWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    padding: 80px 0px 0px 0px;
+    background-color: ${COLOR.DEEP_BLUE_GRAY};
+    @media (max-width: ${BREAKPOINT.MEDIUM}) {
+        padding: 40px 20px 0px 20px;
+      }
+`
