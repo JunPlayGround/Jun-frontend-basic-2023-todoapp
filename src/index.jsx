45d11fc17aa80175d7febc5e5420bdeb2912ac0e
JunPlayGround <j.masaki.23.5.946@4nonome.com>
2023-08-21 01:35:11 +0900

    Revert "/src/index.jsxを作成"
    This reverts commit 356203b5b07761354202577916fcf292fbbc7639.

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,4 @@
-import React from "react";
 import ReactDOM from "react-dom";
-import MainPage from "./components/Pages/MainPage";
 import "./style.css";
 
-ReactDOM.render(<MainPage />, document.getElementById("app"));
+ReactDOM.render(<></>, document.getElementById("app"));
