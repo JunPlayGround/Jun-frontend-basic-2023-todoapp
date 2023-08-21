136dad8205079eca63acf10f6eba9cde11a542ea
JunPlayGround <j.masaki.23.5.946@4nonome.com>
2023-08-22 01:47:45 +0900

    MainPageの参照を変更

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import MainPage from "./components/Pages/MainPage";
+import MainPage from "./components/Pages/MainPage/index";
 import "./style.css";
 
 ReactDOM.render(<MainPage />, document.getElementById("app"));
