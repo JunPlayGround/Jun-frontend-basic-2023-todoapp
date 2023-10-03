772e90cd31693d8daa86007ff9f93a4d9fe9d312
JunPlayGround <j.masaki.23.5.946@4nonome.com>
2023-10-04 03:07:20 +0900

    set+closeAlertをAlertManagerないで実装できた

--- src/components/Organisms/AlertManager/index.jsx
+++ src/components/Organisms/AlertManager/index.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useEffect } from "react";
+import React, { useRef, useState, useEffect, useCallback } from "react";
 import styled from "styled-components";
 import { useAlertHandlerContext } from "../../../contexts/alert_handler";
 import Alert from "../../Atoms/Alert";
@@ -6,8 +6,14 @@ import Alert from "../../Atoms/Alert";
 const AlertManager = () => {
   /* コンポーネント関数内 */
   //Contextを取得
-  const { visible, errorText, setAlert} = useAlertHandlerContext();
+  const { visible, errorText, setAlert, closeAlert} = useAlertHandlerContext();
   console.log("Rerender AlertManager");
+
+  if (visible===true) {
+    setTimeout(() => {
+      closeAlert();
+    }, 5000);
+  }
 
   return (
     <AlertWrapper>
--- src/contexts/alert_handler.jsx
+++ src/contexts/alert_handler.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useState } from "react";
+import React, { createContext, memo, useCallback, useContext, useState } from "react";
 import Alert from "../components/Atoms/Alert";
 import AlertManager from "../components/Organisms/AlertManager";
 
@@ -14,25 +14,24 @@ export const AlertHandlerProvider = ({ children }) => {
   const setAlert = (errorText) => {
     //Alert に表示する内容を指定し、Alert を開く関数
     setAlertState({visible: true, errorText: errorText});
-    // console.log(alertState.visible+" "+alertState.errorText);
+    console.log("setAlert "+alertState.visible+" "+alertState.errorText);
 
-    setTimeout(() => { setAlertState({visible: false, errorText: errorText}); }, 5000);
+    // setTimeout(() => { setAlertState({visible: false, errorText: errorText}); }, 5000);
   };
 
-  // const closeAlert = () => {
-  //   //Alert を閉じる関数
-  //   let text = alertState.errorText;
-  //   setAlertState({visible: false, errorText: text});
-  //   // console.log(alertState.visible+" "+alertState.errorText);
-  // };
-
+  const closeAlert = useCallback(()=>{
+    //Alert を閉じる関数
+    // let text = alertState.errorText;
+    setAlertState({visible: false, errorText: alertState.errorText});
+    console.log("closeAlert "+alertState.visible+" "+alertState.errorText);
+  }, [alertState.visible]);
 
   console.log(alertState);
 
   const contextValue = {
     ...alertState,
     setAlert: setAlert,
-    // closeAlert: closeAlert,
+    closeAlert: closeAlert,
   };
 
   return <AlertHandlerContext.Provider value={contextValue}>{children}</AlertHandlerContext.Provider>;
